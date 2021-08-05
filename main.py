5c6688c5b8a45731eb3dc73acc63bbc4d3a18d3a
Giyu <72243953+afnan007a@users.noreply.github.com>
2021-08-05 07:23:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, request
+from random import choice
 
-app = Flask(__name__)
+web_site = Flask(__name__)
 
+number_list = [
+	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
+	416, 417, 418, 421, 422, 423, 424, 425, 426,
+	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
+]
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@web_site.route('/')
+def random_page():
+  return render_template('index.html', code=choice(number_list))
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+web_site.run(host='0.0.0.0', port=8080)
